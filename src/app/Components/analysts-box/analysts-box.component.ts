795e9ddb15e1621a053123475332cd787ced0b04
fouadMohamed <fm5669653@gmail.com>
2024-12-11 20:42:49 +0200

    adding home page components and modifed the configration

---
+++ src/app/Components/analysts-box/analysts-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysts-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './analysts-box.component.html',
+  styleUrl: './analysts-box.component.scss'
+})
+export class AnalystsBoxComponent {
+
+}
