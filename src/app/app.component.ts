f986caefcfad6495be82e7d4d3d6a3d849ec050d
fouadMohamed <fm5669653@gmail.com>
2024-12-29 19:07:31 +0200

    update angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import 'aos/dist/aos.css';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Hewar.Client';
