37f3a0e272f9b95d27ef7d07e1e10ac8f645cab9
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-02-16 07:35:43 +0200

    create the services, models, form component

--- src/app/features/contracts/models/iget-contract-template-by-id.ts
+++ src/app/features/contracts/models/iget-contract-template-by-id.ts
@@ -1,42 +1,7 @@
-<<<<<<< HEAD
 import { IPriceOfferOtherService } from '../../my-offers/models/iprice-offer-other-service';
 import { IPriceOfferService } from '../../my-offers/models/iprice-offer-service';
-import { IClause } from './iclause';
-import { IContractKey } from './icontract-key';
-import { IScheduleEntry } from './ischedule-entry';
 
-export interface IContractTemplate {
-  contractId: number;
-  offerDate: string;
-  offerNumber: number;
-  facilitySignature: string | null;
-  companySignature: string | null;
-  staticContractTemplate: StaticContractTemplate;
-  staticClauses: StaticClauses[];
-  customClauses: IClause[];
-  contractKeys: IContractKey[];
-  otherServices: IPriceOfferOtherService[];
-  services: IPriceOfferService[];
-  scheduleEntries: IScheduleEntry[];
-}
-export interface StaticClauses {
-  titleAr: string;
-  titleEn: string;
-  contentAr: string;
-  contentEn: string;
-}
-export interface StaticContractTemplate {
-  titleAr: string;
-  titleEn: string;
-  preambleAr: string;
-  preambleEn: string;
-  closingRemarkAr: string;
-  closingRemarkEn: string;
-=======
-import { IPriceOfferOtherService } from "../../my-offers/models/iprice-offer-other-service";
-import { IPriceOfferService } from "../../my-offers/models/iprice-offer-service";
-
-export interface IContractTemplate {
+export interface IGetContractTemplateById {
   contractTitle: {
     ar: string;
     en: string;
@@ -108,5 +73,4 @@ export interface IContractTemplate {
     partyOneSignature: string;
     partyTwoSignature: string;
   };
->>>>>>> 1f61da8 (create the services, models, form component)
 }
