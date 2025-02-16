246b601401b4a9489a297507e03f3791e2e3a40c
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-02-16 07:40:53 +0200

    fix error

--- src/app/features/contracts/components/contract-form/contract-form.component.ts
+++ src/app/features/contracts/components/contract-form/contract-form.component.ts
@@ -11,8 +11,8 @@ import { ContractsService } from '../../services/contracts.service';
 import { ActivatedRoute } from '@angular/router';
 import { IFillContract } from '../../models/ifill-contract';
 import { CommonModule } from '@angular/common';
-import { IGetContractFieldsByOfferId } from '../../models/iget-contract-fields-by-offer-id';
 import { IUpdateContract } from '../../models/iupdate-contract';
+import { IContract } from '../../models/icontract';
 
 @Component({
   selector: 'app-contract-form',
@@ -178,7 +178,7 @@ export class ContractFormComponent implements OnInit {
       }
     }
   }
-  assignValues(res: IGetContractFieldsByOfferId) {
+  assignValues(res: IContract) {
     console.log(res);
 
     this.contractForm.patchValue({
