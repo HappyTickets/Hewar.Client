50c23a2932ded4fb34fe133f3dac0ff886bed97a
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-02-19 18:46:42 +0200

    contract V1

--- src/app/features/contracts/models/icontract.ts
+++ src/app/features/contracts/models/icontract.ts
@@ -3,4 +3,6 @@ import { IContractKey } from './icontract-key';
 export interface IContract {
   contractId: number;
   contractKeys: IContractKey[];
+  contractId: number;
+  contractKeys: IContractKey[];
 }
