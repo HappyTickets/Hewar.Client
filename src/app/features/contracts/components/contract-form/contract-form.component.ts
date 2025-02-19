d620ad5ed9288bb07e79dd9101b70389b2f78e76
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-02-19 18:46:42 +0200

    contract V1

--- src/app/features/contracts/components/contract-form/contract-form.component.ts
+++ src/app/features/contracts/components/contract-form/contract-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputComponent } from '../../../../shared/components/input/input.component';
@@ -10,49 +14,26 @@ import { CommonModule } from '@angular/common';
 import { catchError, EMPTY, tap } from 'rxjs';
 import { ContractService } from '../../services/contract.service';
 import { IContractFields } from '../../models/icontract-fields';
-=======
-import { FormGroup, FormBuilder, Validators, FormArray, ReactiveFormsModule, FormControl } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { DatePickerModule } from 'primeng/datepicker';
-import { InputTextModule } from 'primeng/inputtext';
-import { TableModule } from 'primeng/table';
-import { InputComponent } from '../../../../shared/components/input/input.component';
-import { TranslatePipe } from '@ngx-translate/core';
-import { ContractsService } from '../../services/contracts.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { IFillContract } from '../../models/ifill-contract';
-import { CommonModule } from '@angular/common';
-import { IContract } from '../../models/icontract';
-import { catchError, EMPTY, tap } from 'rxjs';
-import { IUpdateContract } from '../../models/iupdate-contract';
 
 @Component({
   selector: 'app-contract-form',
   standalone: true,
-<<<<<<< HEAD
-  imports: [ ReactiveFormsModule, CardModule, ButtonModule, InputComponent, TranslatePipe, CommonModule ],
-=======
-  imports: [ReactiveFormsModule, CardModule, TableModule, ButtonModule, InputTextModule, DatePickerModule, InputComponent, TranslatePipe, CommonModule],
->>>>>>> 1f61da8 (create the services, models, form component)
+  imports: [
+    ReactiveFormsModule,
+    CardModule,
+    ButtonModule,
+    InputComponent,
+    TranslatePipe,
+    CommonModule,
+  ],
   templateUrl: './contract-form.component.html',
   styleUrl: './contract-form.component.scss',
 })
 export class ContractFormComponent implements OnInit {
-<<<<<<< HEAD
   private contractService = inject(ContractService);
   private route = inject(ActivatedRoute);
   private fb = inject(FormBuilder);
   private router = inject(Router);
-  private router = inject(Router);
-  date = new Date(Date.now());
-  mode: 'create' | 'update' = 'create';
-  contractForm!: FormGroup;
-  contractId = 0;
-=======
-  private contractsService = inject(ContractsService);
-  private route = inject(ActivatedRoute);
-  private fb = inject(FormBuilder);
   private router = inject(Router);
   date = new Date(Date.now());
   mode: 'create' | 'update' = 'create';
@@ -63,26 +44,28 @@ export class ContractFormComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.offerId = +(params.get('id') ?? 0);
-      this.contractsService.getContractFieldsByOfferId(this.offerId).pipe(
-        tap((res) => {
-          if (res.data) {
-            if (res.data.contractId) this.contractId = res.data.contractId;
-            this.mode = 'update';
-            this.assignValues(res.data);
-          }
-        }),
-        catchError(() => {
-          this.mode = 'create';
-          return EMPTY;
-        })
-      ).subscribe();
+      this.contractService
+        .GetContractFieldsByOfferId(this.offerId)
+        .pipe(
+          tap((res) => {
+            if (res.data) {
+              if (res.data.contractId) this.contractId = res.data.contractId;
+              this.mode = 'update';
+              this.assignValues(res.data);
+            }
+          }),
+          catchError(() => {
+            this.mode = 'create';
+            return EMPTY;
+          })
+        )
+        .subscribe();
     });
   }
   constructor() {
     this.contractForm = this.fb.group({
       contractSignDate: ['', Validators.required],
       contractStartDate: ['', Validators.required],
-<<<<<<< HEAD
 
       companyNameAr: ['', Validators.required],
       companyNameEn: ['', Validators.required],
@@ -131,11 +114,15 @@ export class ContractFormComponent implements OnInit {
   submitContract() {
     if (this.contractForm.valid) {
       if (this.mode === 'create') {
-        this.contractService.CreateContractForOffer(this.contractForm.value, this.offerId).subscribe(() => {
-          this.router.navigate([`contract-preview/${this.offerId}`]);
-        });
+        this.contractService
+          .CreateContractForOffer(this.contractForm.value, this.offerId)
+          .subscribe(() => {
+            this.router.navigate([`contract-preview/${this.offerId}`]);
+          });
       } else {
-        this.contractService.UpdateContractByFields(this.contractForm.value, this.contractId).subscribe(() => {
+        this.contractService
+          .UpdateContractByFields(this.contractForm.value, this.contractId)
+          .subscribe(() => {
             this.router.navigate([`contract-preview/${this.offerId}`]);
           });
       }
@@ -143,181 +130,5 @@ export class ContractFormComponent implements OnInit {
   }
   assignValues(res: IContractFields) {
     this.contractForm.patchValue(res.contractFields);
-=======
-      partyOne: this.fb.group({
-        name: this.fb.group({ ar: ['', Validators.required], en: ['', Validators.required] }),
-        mainOfficeCity: this.fb.group({ ar: ['', Validators.required], en: ['', Validators.required] }),
-        publicSecurityLicense: ['', Validators.required],
-        telephone: ['', Validators.required],
-        mobile: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        nationalAddress: this.fb.group({
-          city: this.fb.group({
-            ar: ['', Validators.required],
-            en: ['', Validators.required],
-          }),
-          postalCode: ['', Validators.required],
-          unitNumber: ['', Validators.required],
-          buildingNumber: ['', Validators.required],
-        }),
-        representativeName: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        representativeTitle: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        commercialRegistration: ['', Validators.required],
-        registrationInSabl: ['', Validators.required],
-        guardsCount: [0, Validators.required],
-      }),
-      partyTwo: this.fb.group({
-        name: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        mainOfficeCity: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        publicSecurityLicense: ['', Validators.required],
-        telephone: ['', Validators.required],
-        mobile: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        nationalAddress: this.fb.group({
-          city: this.fb.group({
-            ar: ['', Validators.required],
-            en: ['', Validators.required],
-          }),
-          postalCode: ['', Validators.required],
-          unitNumber: ['', Validators.required],
-          buildingNumber: ['', Validators.required],
-        }),
-        representativeName: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        representativeTitle: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        commercialRegistrationCity: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        locationToBeSecured: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-      }),
-      scheduleEntries: this.fb.array([]),
-      customClauses: this.fb.array([]),
-    });
-  }
-  get scheduleEntries(): FormArray {
-    return this.contractForm.get('scheduleEntries') as FormArray;
-  }
-  get customClauses(): FormArray {
-    return this.contractForm.get('customClauses') as FormArray;
-  }
-  addScheduleEntry() {
-    const scheduleEntries = this.contractForm.get('scheduleEntries') as FormArray;
-    scheduleEntries.push(
-      this.fb.group({
-        location: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        guardsRequired: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        shiftTime: this.fb.group({
-          ar: ['', Validators.required],
-          en: ['', Validators.required],
-        }),
-        notes: this.fb.group({
-          ar: [''],
-          en: [''],
-        }),
-      })
-    );
-  }
-  removeScheduleEntry(index: number) {
-    const scheduleEntries = this.contractForm.get('scheduleEntries') as FormArray;
-    scheduleEntries.removeAt(index);
-  }
-  addCustomClause() {
-    const customClauses = this.contractForm.get('customClauses') as FormArray;
-    customClauses.push(
-      this.fb.group({
-        ar: ['', Validators.required],
-        en: ['', Validators.required],
-      })
-    );
-  }
-  removeCustomClause(index: number) {
-    const customClauses = this.contractForm.get('customClauses') as FormArray;
-    customClauses.removeAt(index);
-  }
-  getPartyField(field: string): FormControl {
-    return this.contractForm.get(field) as FormControl || new FormControl('');
-  }
-  submitContract() {
-    if (this.contractForm.valid) {
-      if (this.mode === 'create') {
-        const form: IFillContract = {contract: this.contractForm.value, offerId:this.offerId}
-        this.contractsService.fillFields(form).subscribe(() => {
-          this.router.navigate([`contract-preview/${this.contractId}`]);
-        });
-      } else {
-        const form: IUpdateContract = {contract: this.contractForm.value, id: this.contractId}
-        this.contractsService.updateFields(form).subscribe(() => {
-          this.router.navigate([`contract-preview/${this.contractId}`]);
-        });
-      }
-    }
-  }
-  assignValues(res: IContract) {
-    this.contractForm.patchValue({
-      partyOne: res.partyOne,
-      partyTwo: res.partyTwo,
-      contractSignDate: new Date(res.contractSignDate),
-      contractStartDate: new Date(res.contractStartDate),
-    });
-    if (res.scheduleEntries && res.scheduleEntries.length > 0) {
-      res.scheduleEntries.forEach(entry => {
-        const scheduleEntryGroup = this.fb.group({
-          location: this.fb.group({
-            ar: [entry.location.ar, Validators.required],
-            en: [entry.location.en, Validators.required],
-          }),
-          guardsRequired: this.fb.group({
-            ar: [entry.guardsRequired.ar, Validators.required],
-            en: [entry.guardsRequired.en, Validators.required],
-          }),
-          shiftTime: this.fb.group({
-            ar: [entry.shiftTime.ar, Validators.required],
-            en: [entry.shiftTime.en, Validators.required],
-          }),
-          notes: this.fb.group({
-            ar: [entry.notes.ar],
-            en: [entry.notes.en],
-          }),
-        });
-        this.scheduleEntries.push(scheduleEntryGroup);
-      });
-    }
-    if (res.customClauses && res.customClauses.length > 0) {
-      res.customClauses.forEach(clause => {
-        const customClauseGroup = this.fb.group({
-          ar: [clause.ar, Validators.required],
-          en: [clause.en, Validators.required],
-        });
-        this.customClauses.push(customClauseGroup);
-      });
-    }
->>>>>>> 1f61da8 (create the services, models, form component)
   }
 }
--- src/app/features/contracts/models/icontract-template.ts
+++ src/app/features/contracts/models/icontract-template.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { IPriceOfferOtherService } from '../../my-offers/models/iprice-offer-other-service';
 import { IPriceOfferService } from '../../my-offers/models/iprice-offer-service';
 import { IClause } from './iclause';
@@ -32,81 +31,4 @@ export interface StaticContractTemplate {
   preambleEn: string;
   closingRemarkAr: string;
   closingRemarkEn: string;
-=======
-import { IPriceOfferOtherService } from "../../my-offers/models/iprice-offer-other-service";
-import { IPriceOfferService } from "../../my-offers/models/iprice-offer-service";
-
-export interface IContractTemplate {
-  contractTitle: {
-    ar: string;
-    en: string;
-  };
-  preamble: {
-    title: {
-      ar: string;
-      en: string;
-    };
-    parties: {
-      firstParty: {
-        description: {
-          ar: string;
-          en: string;
-        };
-      };
-      secondParty: {
-        description: {
-          ar: string;
-          en: string;
-        };
-      };
-    };
-    introduction: {
-      ar: string;
-      en: string;
-    };
-    conditions: {
-      ar: string;
-      en: string;
-    }[];
-  };
-  clauses: {
-    number: number;
-    title: {
-      ar: string;
-      en: string;
-    };
-    ar: string;
-    en: string;
-  }[];
-  customClauses: {
-    ar: string;
-    en: string;
-  }[];
-  scheduleEntries: {
-    location: {
-      ar: string;
-      en: string;
-    };
-    guardsRequired: {
-      ar: string;
-      en: string;
-    };
-    shiftTime: {
-      ar: string;
-      en: string;
-    };
-    notes: {
-      ar: string;
-      en: string;
-    };
-  }[];
-  servicesOffer: IPriceOfferService[];
-  otherServicesOffer: IPriceOfferOtherService[];
-  duties_Services: { ar: string; en: string }[];
-  closingRemark: { ar: string; en: string };
-  signatures: {
-    partyOneSignature: string;
-    partyTwoSignature: string;
-  };
->>>>>>> 1f61da8 (create the services, models, form component)
 }
