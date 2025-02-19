331233a606e7684e6e5afbc5f682d41cf23b72fa
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-02-19 18:46:42 +0200

    contract V1

--- src/app/features/contracts/models/icontract-template.ts
+++ src/app/features/contracts/models/icontract-template.ts
@@ -1,76 +1,34 @@
-import { IPriceOfferOtherService } from "../../my-offers/models/iprice-offer-other-service";
-import { IPriceOfferService } from "../../my-offers/models/iprice-offer-service";
+import { IPriceOfferOtherService } from '../../my-offers/models/iprice-offer-other-service';
+import { IPriceOfferService } from '../../my-offers/models/iprice-offer-service';
+import { IClause } from './iclause';
+import { IContractKey } from './icontract-key';
+import { IScheduleEntry } from './ischedule-entry';
 
 export interface IContractTemplate {
-  contractTitle: {
-    ar: string;
-    en: string;
-  };
-  preamble: {
-    title: {
-      ar: string;
-      en: string;
-    };
-    parties: {
-      firstParty: {
-        description: {
-          ar: string;
-          en: string;
-        };
-      };
-      secondParty: {
-        description: {
-          ar: string;
-          en: string;
-        };
-      };
-    };
-    introduction: {
-      ar: string;
-      en: string;
-    };
-    conditions: {
-      ar: string;
-      en: string;
-    }[];
-  };
-  clauses: {
-    number: number;
-    title: {
-      ar: string;
-      en: string;
-    };
-    ar: string;
-    en: string;
-  }[];
-  customClauses: {
-    ar: string;
-    en: string;
-  }[];
-  scheduleEntries: {
-    location: {
-      ar: string;
-      en: string;
-    };
-    guardsRequired: {
-      ar: string;
-      en: string;
-    };
-    shiftTime: {
-      ar: string;
-      en: string;
-    };
-    notes: {
-      ar: string;
-      en: string;
-    };
-  }[];
-  servicesOffer: IPriceOfferService[];
-  otherServicesOffer: IPriceOfferOtherService[];
-  duties_Services: { ar: string; en: string }[];
-  closingRemark: { ar: string; en: string };
-  signatures: {
-    partyOneSignature: string;
-    partyTwoSignature: string;
-  };
+  contractId: number;
+  offerDate: string;
+  offerNumber: number;
+  facilitySignature: string | null;
+  companySignature: string | null;
+  staticContractTemplate: StaticContractTemplate;
+  staticClauses: StaticClauses[];
+  customClauses: IClause[];
+  contractKeys: IContractKey[];
+  otherServices: IPriceOfferOtherService[];
+  services: IPriceOfferService[];
+  scheduleEntries: IScheduleEntry[];
+}
+export interface StaticClauses {
+  titleAr: string;
+  titleEn: string;
+  contentAr: string;
+  contentEn: string;
+}
+export interface StaticContractTemplate {
+  titleAr: string;
+  titleEn: string;
+  preambleAr: string;
+  preambleEn: string;
+  closingRemarkAr: string;
+  closingRemarkEn: string;
 }
---
+++ src/app/features/contracts/models/iedit-schedule-entry.ts
@@ -0,0 +1,10 @@
+export interface IEditScheduleEntry {
+  locationAr: string;
+  locationEn: string;
+  guardsRequired: number;
+  shiftTimeAr: string;
+  shiftTimeEn: string;
+  notesAr: string;
+  notesEn: string;
+  id?: number;
+}
